62525b2c88f207f78a8b7d94f3e237c18a26cb3d
alexs55 <alex.acosta.dv@gmail.com>
2019-03-23 21:50:57 -0500

    first push

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+    "name": "Ahmed Ali",
+    "photo": "https://hokiesports.com/images/2018/9/18/Hill_Ahmed_19MB_HEAD_46.jpg?width=300",
+    "scores": [
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1",
+    "1"
+    ]
+    },
+    {
+    "name": "Julio Lopez",
+    "photo": "https://www.askdennisbrown.com/wp-content/uploads/2018/07/Josue-Pena.jpg",
+    "scores": [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+    ]
+    },
+    {
+    "name": "Jeremy Bryant",
+    "photo": "https://vargasgonzalez.com/wp-content/uploads/2018/05/Josue.Espinosa_6056_web-1-700x876.jpg",
+    "scores": [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+    ]
+    },
+    {
+    "name": "Luis T Hamilton",
+    "photo": "https://vignette.wikia.nocookie.net/dexter/images/3/3b/Josue_Aguirre_3.jpg/revision/latest?cb=20181006171854",
+    "scores": [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+    ]
+    },
+    {
+    "name": "Larry Vega",
+    "photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTBgv7QMnQV-WzMBEcagwf5oLj3V5Y5snDCx6Pq9HVbamo7TIJ4A",
+    "scores": [
+    "5",
+    "5",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "5",
+    "1",
+    "4"
+    ]
+    },
+    {
+    "name": "Zeke Greaves",
+    "photo": "http://melissajdixon.com/wp-content/uploads/2015/11/josue_closeup.jpg",
+    "scores": [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+    ]
+    }
+    
+    ];
+    
+     module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,40 @@
+var friends = require('../data/friends.js');
+
+
+
+module.exports = function (app) {
+
+
+  app.get("/api/friends", function (req, res) {
+    return res.json(friends);
+  });
+
+
+
+
+  // Create New friends - takes in JSON input
+  app.post("/api/friends", function (req, res) {
+    var newfriend = req.body;
+    var friendmatch = 100;
+    var selected;
+    console.log("added new friend");
+    for (var i = 0; i < friends.length; i++) {
+      var match = friends[i]
+      var result = 0;
+      for (var x = 0; x < match.scores.length; x++) {
+        console.log(Math.abs(newfriend.scores[x] - match.scores[x]))
+        result += Math.abs(newfriend.scores[x] - match.scores[x]);
+      }
+      if (result < friendmatch) {
+        console.log(result);
+        friendmatch = result;
+        selected = match;
+      }
+    }
+    console.log(selected);
+    console.log(newfriend);
+    res.send(selected);
+    friends.push(newfriend)
+  });
+
+};
